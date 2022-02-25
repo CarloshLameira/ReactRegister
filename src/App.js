31446656875bcd9fa29d96c847269fb07487bd22
CarloshLameira <carloshlameira@hotmail.com>
2022-02-25 10:57:22 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Formulario from './components/Formulario/Formulario'
+import Container from '@mui/material/Container'
+import { Typography } from '@mui/material';
+import '@fontsource/roboto/500.css'
+function App() {
+  return (
+          <Container component="article" maxWidth = "sm" >
+        <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
+        <Formulario onSubmit ={onSubmitForm} validarCPF={validarCPF}/>
+        </Container>
+  );
+}
+function onSubmitForm(dados){
+        console.log(dados)
+}
+function validarCPF(cpf){
+        if (cpf.length!==11){
+                return {valido:false, texto:"O cpf deve possuir 11 digitos"}
+        }else{
+                return{valido:true, texto:""}
+        }
+}
+export default App;
